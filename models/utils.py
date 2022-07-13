681484abfc224fb4ec3eab8cab4db4637e76b9d7
jean-noel <jeannoel.bonvent@jinnov-insa.fr>
2022-07-13 13:13:17 +0200

    form_mmail_commentaires

--- models/utils.py
+++ models/utils.py
@@ -1,16 +1,18 @@
 import smtplib, ssl
 
-def mail_manager(message):
-    """Procedure envoyant un e-mail vers une boite mail dédié"""
-
-    port = 465 
+def mail_manager(message:str, sender_email:str, receiver_email:str, connection_token:str):
+    """
+    This function sends an email to the receiver_email address with the message.
+    
+    :param message: The message to be sent
+    :param sender_email: The email address of the sender
+    :param receiver_email: The email address of the person you want to send the email to
+    :param connection_token: This is the application token of the email address you're sending from
+    """
+    port = 465
     smtp_server = "smtp.gmail.com"
-    sender_email = "mail"  # Enter your address
-    receiver_email = "mail"  # Enter receiver address
-    password = "token" #mdp d'application généré depuis google
-
-
+    
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
+        server.login(sender_email,  connection_token)
         server.sendmail(sender_email, receiver_email, message)
