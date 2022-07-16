d7ad458342f6014519af265b881f1bac41e5fc1d
axell <axel.lenroue@gmail.com>
2022-07-16 16:21:26 +0200

    add user path

--- jinnov-site.sql
+++ jinnov-site.sql
@@ -126,6 +126,32 @@ LOCK TABLES `membre` WRITE;
 /*!40000 ALTER TABLE `membre` DISABLE KEYS */;
 /*!40000 ALTER TABLE `membre` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_path`
+--
+
+DROP TABLE IF EXISTS `user_path`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_path` (
+  `id` varchar(45) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `page` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_path`
+--
+
+LOCK TABLES `user_path` WRITE;
+/*!40000 ALTER TABLE `user_path` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_path` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -136,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-16 16:18:37
+-- Dump completed on 2022-07-16 16:21:08
