fafd1ef03618ebf22089156905bb2c1199e2c10b
fredo <fredo@bobatoII.home>
2022-08-02 19:52:15 +0200

    merged mobile and desktop version

--- test_header.py
+++ test_header.py
@@ -8,10 +8,5 @@ app = Flask("__name__")
 def accueil():
     return flask.render_template('base.html')
 
-@app.route('/mobile')
-def acceuil_mobile():
-    return flask.render_template('base_mobile.html')
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
