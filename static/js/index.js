bc5e112cd980e7d653ccf3325f220bd02b1e4c8c
fredo <fredo@bobatoII.home>
2022-08-12 14:24:05 +0200

    implemented 2 first block for desktop

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+button2 = document.getElementById('scroll');
+var state = 0;
+button2.addEventListener('click', function(){
+    console.log('zizi');
+    var h = window.innerHeight; 
+    window.scroll(0,h*1.09);
+});
