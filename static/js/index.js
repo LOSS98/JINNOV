3c5d1bce23c45ba7185d7bc2db6a41dc0f70f10e
fredo <fredo@bobatoII.home>
2022-08-12 20:39:31 +0200

    implemented 3rd block for full view

--- static/js/index.js
+++ static/js/index.js
@@ -3,5 +3,5 @@ var state = 0;
 button2.addEventListener('click', function(){
     console.log('zizi');
     var h = window.innerHeight; 
-    window.scroll(0,h*1.09);
+    window.scroll(0,h*1.12);
 });
